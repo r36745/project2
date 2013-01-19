ffa2548c1658fa71cb2e9ca1ef7a2efc9a073580
Steven Roseman <stevierose@fullsail.edu>
2013-01-19 17:45:54 -0500

    Added updated files.

---
+++ steven-roseman-project2.js
@@ -0,0 +1,114 @@
+//alert("JavaScript works!");
+
+// Steven Roseman II
+// SDI 1301
+// Project 2
+// Flow Chart
+
+
+// My Variables
+var sonExpression = "smile"
+var tankLevel = "empty";
+var tripDestination = "Pizza Hut";
+var buyGallonsGas = 10; 
+var foodIdeas = ["steak", "fish", "chicken", "potates", "rice"]; // available food
+var servingSize = 5;
+var walletFull = true;
+
+
+//  Procedure
+
+function haveFun(reaction) {
+    if (reaction == "smile") {
+        console.log("had fun");
+    } else {
+        console.log("did not have fun");
+    };
+};
+
+var park = haveFun(sonExpression);
+
+
+//Boolean Function
+
+function needGas (fuelLevel, hasMoney){
+    
+	//need gas if tank empty and I have money to pay for it
+	if ((fuelLevel =="empty")&&(hasMoney)){
+		return true ;
+	} else {
+                return false;
+		
+	}
+};
+
+var buyGas = needGas(tankLevel, walletFull); 
+
+console.log(buyGas);
+
+
+// String
+
+function checkCar (fuelLevel, destination){
+	
+	if (fuelLevel =="full"){
+		return "You can take the car to " + destination;
+	}else 
+	if (fuelLevel =="halfway"){
+		return "You may have enough gas to make it to the " + destination;
+	}else{
+		return "Man, I don't have any gas; we'll have to walk to the " + destination + " in this heat";
+	}
+}
+
+var transportation = checkCar (tankLevel, tripDestination);
+
+console.log(transportation);
+
+
+// Number function
+
+function getGas(totalGallons) {
+    var pricePerGallon = 3.75;
+    var total = 0;
+    
+// rounding price of gas to nearest dollar
+while (totalGallons > 0){
+    total += Math.round(pricePerGallon);
+    totalGallons--;
+    }
+     return total;
+    
+};
+var getTotal = getGas (buyGallonsGas);
+console.log(getTotal);
+
+
+//Array
+
+function dinnerChoices(foodList, servings) {
+    
+    var dinnerMenu = new Array();
+    var tempChoice = 0;
+    
+    // limiting the amount of serving to one for each menu item
+    if (servings > foodList.length){
+        servings = foodList.length;
+    }
+    for (var i = 0; i < foodList.length; i++) {
+        tempChoice = Math.floor((Math.random() * servings));
+        dinnerMenu.push(foodList[tempChoice]);
+    }
+    return dinnerMenu;
+};
+
+var dinner = dinnerChoices(foodIdeas,servingSize);
+console.log(dinner);
+
+
+
+
+
+
+
+
