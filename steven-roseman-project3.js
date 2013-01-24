c9d41173e2601ec8cb964c3056434bcdd19c9f98
Steven Roseman <stevierose@fullsail.edu>
2013-01-24 16:58:00 -0500

    add getter and setters. method has a syntax error. working on correction.

--- steven-roseman-project3.js
+++ steven-roseman-project3.js
@@ -1,9 +1,7 @@
-//alert("JavaScript works!");
-
-
 var assignment = {
     name: "Project 3",
     assignmentNumber: 3,
+    grade: "A",
     Requirements: [
         "code",
         "story",
@@ -26,24 +24,46 @@ var assignment = {
     },
     setRequirements: function (allRequirements) {
         this.requirements=allRequirements
-    }
+    },
+    getGrade: function () {
+        return this.grade;
+    },
+    setGrade: function (finalGrade) {
+        this.grade=finalGrade;
+    },
+    
+    startAssignment: function () {
+        
+    var hw = getAssignmentNumber;
        
+    if ( hw === 1) {
+        "setAssignNumber";
+    },
+    if ( hw === 2 ) {
+        "setAssignmentNumber";
+    },
+    if ( hw === 3 ) {
+        "Turn in Project";
+    } else {
+        "look again"
 };
 
 assignment.setAssignmentNumber(6);
 assignment.setAssignmentName("Last Project");
 assignment.setRequirements("code, Story on Project 3, Hopefully followed requirements");
+assignment.setGrade("A");
+
 
 var test = assignment.getAssignmentNumber();
 var newTest = assignment.getAssignmentName();
 var nextTest = assignment.getRequirements();
+var finalTest = assignment.getGrade();
 
 
 console.log(test);
 console.log(newTest);
 console.log(nextTest);
-
-
+console.log(finalTest);
 
 
 
