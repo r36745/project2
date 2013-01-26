c20d29e0b3f21e2ef3572b0239897d5dd62f7f3f
Steven Roseman <stevierose@fullsail.edu>
2013-01-26 17:41:35 -0500

    Added gets and set for Jamaica trip code.

--- steven-roseman-project3.js
+++ steven-roseman-project3.js
@@ -1,3 +1,6 @@
+// Steven Roseman
+// Project 3
+// Create Flow Chart
 
 
 // In Jamaica my wife and I stayed at a beautiful bed and breakfast hotel; that was on the cliffs, our hotel room was 40 ft from the water.
@@ -9,6 +12,30 @@ var hotel = {
                 employees: ["Teddy", " Petrona", "Linda"],
                 amenities: "Room Service",
                 name: "Blue Cave Castle",
+                getRoomType : function () {
+                    return this.roomType;
+                },
+                setRoomType: function (roomView) {
+                    this.roomType = roomView;
+                },
+                getAddress: function () {
+                    return this.address;
+                },
+                setAddress: function (hotelLocation) {
+                    this.address = hotelLocation;
+                },
+                getEmployees: function () {
+                    return this.employees;
+                },
+                setEmployees: function (awesomePeople) {
+                    this.employees = awesomePeople;
+                },
+                getName: function () {
+                    return this.name;
+                },
+                setName: function (hotelName) {
+                    this.name = hotelName;
+
                 
                 // Procedure
                 
@@ -20,13 +47,33 @@ var hotel = {
                     console.log( "Not my favorite room, but it will do.");
                     }
                     
+                },
+                       
+      // Function
+                
+                getAmenities: function() {
+                    return this.amenities;
+                },
+                setHotelAmenities: function(roomService) {
+                    if (roomService == "Delicious" || roomService == "just right") { 
+                    
+                        } else if (roomService == "oneMore" && roomService == "the best") {
+                        } else {
+                         }
+                         this.amenities = roomService;
                 }
+                            
+                        
+                               
 }
 
-
-
+hotel.setHotelAmenities("Delicious", "just right");
+console.log(hotel.getHotelAmenities);
 
 hotel.roomQuality("Superior Room");
+
+
+
 
 
 
