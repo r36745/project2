1ef7afa16552f28de705cb8d905b2346e7ef52c6
Steven Roseman <stevierose@fullsail.edu>
2013-01-26 21:02:22 -0500

    added two more methods, getting error trying to print list.

--- steven-roseman-project3.js
+++ steven-roseman-project3.js
@@ -4,11 +4,19 @@
 
 //myVariables
 
+var attractions = new Array(); //one way to create a array
+attractions[0] = "Ricks Cafe"; // setting an array element
+attractions[1] = "Dolphin Cove";
+attractions[2] = "7 mile beach";
+
+
+
 
 
 
 // In Jamaica my wife and I stayed at a beautiful bed and breakfast hotel; that was on the cliffs, our hotel room was 40 ft from the water.
-
+//Object, following getter/setters, then methods which contain trip data.
+// Also, some of the places we saw.
 
 var hotel = {
                 roomType: "Superior Room",
@@ -42,7 +50,6 @@ var hotel = {
                 },
                 
                 // Procedure
-                
                 roomQuality: function (roomName) {
                 
                     if (roomName == "Superior Room") {
@@ -54,7 +61,7 @@ var hotel = {
                 },
                        
                 // Function
-                // Nested Loop
+                // Silly function, but wanted to show nested loop
                 
                 hotelAmenities: function (roomService, rightAmount, excellentCustService) {
                     for (i = roomService; i <= rightAmount; i++) {
@@ -62,10 +69,37 @@ var hotel = {
                         for (j = roomService; j <= excellentCustService; j++) {
                         console.log("I've never had jerk lobster before, this taste fabulous! My compliments to " + hotel.employees[0]);
                         }
-                    }        
-                }       
+                    }
+                    return "This hotel has the perfect amenities";
+                },      
                  
+                
+                
+                // Number
+                
+                beachWaterTemp: function (tempValue, tempType) {
+                var convertedTemp = 0;
+                    
+                    if (tempType == 'C' || tempType =='c') {
+                        convertedTemp = tempValue * 9/5 +32;
+                    } else if ( tempType == 'F' || tempType == 'f') {
+                        convertedTemp = (tempValue - 32) * 5/9;
+                    } else {
+                        convertedTemp = -999;
+                    }
+                    return Math.round(convertedTemp);
+                },
+                
                 //
+                
+                printJamaicaAttractions: function (attractionList){  // sample function that takes an array and prints to console each car
+                    var attractionNumber = 0;			// local variable			
+                    console.log(attractionList.length);
+                    while(attractionNumber<attractionList.length){		// loops from 0 to carList.length-1
+		console.log(attractionList[attractionNumber]); // prints the car at index i
+		attractionNumber++;
+                    }
+                }
                 
                 
 }
@@ -73,6 +107,13 @@ var hotel = {
 
 hotel.roomQuality("Superior Room");
 hotel.hotelAmenities(0,5,5);
+console.log("32 degrees Celsius is Jamaica water temp " + hotel.beachWaterTemp(32,"c") + " degrees Fahrenheit");	// using the function and printing its output
+console.log(" Jamaica's weather 100 degrees, Fahrenheit is " +  hotel.beachWaterTemp(100, 'F') + " degrees Celsius");
+console.log("150 degrees Fahrenheit is " +  hotel.beachWaterTemp(100, 'x') + " degrees Celsius");  // using invalid tempType
+
+console.log("This is the print of Negril attractions; you requested:");
+
+hotel.printJamaicaAtrractions(attractions); // just prints out cars2 array
 
 
 
